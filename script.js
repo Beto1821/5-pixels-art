a619d3eee6bc33deff9d6e3bd6284eb3ca5a6f69
Beto1821 <beto1821@uol.com.br>
2022-03-21 19:33:49 -0300

    ex04 rev01

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let pixelBox = document.getElementById('pixel-board');
 
+
 function criaMatriz() {
-  for (let z = 0; z < 5; z += 1) {
-    let quebra = document.createElement('br');
-    pixelBox.appendChild(quebra);
-    for (let i = 0; i < 5; i += 1) {
+    
+  for (let z = 1; z <= 5; z += 1) {
+    let row = document.createElement('div');
+    row.className = 'linha';
+    for (let i = 1; i <= 5; i += 1) {
       let box = document.createElement('div');
       box.className = 'pixel';
-      pixelBox.appendChild(box);
+      row.appendChild(box);
     }
+    pixelBox.appendChild(row);
   }
 }
 criaMatriz();
