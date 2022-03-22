874bf6b912d31450c6e120cc3ed158b74f788752
Beto1821 <beto1821@uol.com.br>
2022-03-22 09:39:31 -0300

    ex08 rev1000

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //item 4
-let paletaCores = document.getElementById('color-palette') 
+let paletaCores = document.getElementById('color-palette');
 let pixelBox = document.getElementById('pixel-board');
 let pixel = document.querySelector('.pixel');
 const Cblack = document.getElementsByClassName('black');
@@ -8,13 +8,13 @@ const Cblue = document.getElementsByClassName('blue');
 const Cgreen = document.getElementsByClassName('green');
 
 function criaMatriz() {
-    
   for (let z = 1; z <= 5; z += 1) {
     let row = document.createElement('div');
     row.className = 'linha';
     for (let i = 1; i <= 5; i += 1) {
       let box = document.createElement('div');
       box.className = 'pixel';
+      box.addEventListener('click', pixelColor);
       row.appendChild(box);
     }
     pixelBox.appendChild(row);
@@ -24,17 +24,30 @@ criaMatriz();
 
 //7
 function seleciona(event) {
-    let eraSelecionada = document.querySelector('.selected')
-    eraSelecionada.classList.remove('selected');
-    event.target.classList.add('selected');
+  let eraSelecionada = document.querySelector('.selected');
+  eraSelecionada.classList.remove('selected');
+  event.target.classList.add('selected');
+  console.log(getComputedStyle(event.target).backgroundColor);
+}
+paletaCores.addEventListener('click', seleciona);
+
+//8
+
+function pixelColor(event) {
+  let paleta = document.querySelector('.selected');
+  let color = getComputedStyle(paleta).backgroundColor;
+  event.target.style.backgroundColor = color;
+  console.log(event.target);
+  console.log(color);
+}
+
+function changeColor() {
+  let pixelBoard = document.querySelectorAll('.pixel');
+  for (i = 0; i < pixelBoard.length; i += 1) {
+    pixelBoard[i].addEventListener('click', pixelColor);
+  console.log('clicou');
+  }
 
 }
-paletaCores.addEventListener('click', seleciona)
-
-// Cblack.addEventListener('click', seleciona)
-// Cred.addEventListener('click', seleciona)
-// Cblue.addEventListener('click', seleciona)
-// Cgreen.addEventListener('click', seleciona)
-
 
 
